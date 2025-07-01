16d9e8d2311892196825cee2f7883b4e5a1a4c39
noah.tseng <t55619@hotmail.com.tw>
2025-07-02 03:23:29 +0800

    CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # ✅ 使用「上一層」的 crawler 資料夾
 json_path = os.path.join(os.path.dirname(__file__),  "crawler", "kaohsiung_restaurants_detailed.json")
